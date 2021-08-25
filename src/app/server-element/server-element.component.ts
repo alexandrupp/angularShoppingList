acfdadf51587a7a91fb2b6918b03ce3ada3bf65d
alexandrupp <alexandrup@ensemble.com>
2021-08-25 09:43:49 +0300

    16. Seeing Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,15 +6,42 @@ import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None, ShadowDom - None applies this component's styles globally
 })
-export class ServerElementComponent implements OnInit {
-  @Input("srvElement") element: {
-    type: string,
-    name: string,
-    content: string
-  };
-  constructor() { }
-
-  ngOnInit(): void {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input("srvElement") element: { type: string, name: string, content: string };
+  @Input() name: string;
+  constructor() {
+    console.log("constructor called!");
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called!");
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit called!");
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck called!")
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called!");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called!");
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called!");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called!");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy called!");
+  }
 }
