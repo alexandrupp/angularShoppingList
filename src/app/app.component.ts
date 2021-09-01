2dba16de2811ce821dddd069d40bd27f214913b0
alexandrupp <alexandrup@ensemble.com>
2021-09-01 10:08:51 +0300

    try to implement navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayRecipes = false;
 
+  showRecipes(data: {recipes: boolean, shoppingList: boolean}) {
+    if (data.recipes === true && data.shoppingList === false)
+      this.displayRecipes = true;
+  }
+  showShoppingList(data: {recipes: boolean, shoppingList: boolean}) {
+    if (data.recipes === false && data.shoppingList === true)
+      this.displayRecipes = false;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core"
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: "app-header",
@@ -7,4 +7,16 @@ import { Component } from "@angular/core"
 
 export class HeaderComponent{
     collapsed = true;
+
+    @Output("emitRecipes") recipesEmitted = new EventEmitter<{recipes: boolean, shoppingList: boolean}>();
+    @Output("emitShoppingList") shoppingListEmitted = new EventEmitter<{recipes: boolean, shoppingList: boolean}>();
+
+    displayRecipes(){
+        this.recipesEmitted.emit({recipes: true, shoppingList: false});
+    }
+
+
+    displayShoppingList(){
+        this.shoppingListEmitted.emit({recipes: false, shoppingList: true});
+    }
 }
