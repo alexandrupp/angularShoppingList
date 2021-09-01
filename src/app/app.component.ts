d499c0bee326b21732dddee047aad138d74bdd30
alexandrupp <alexandrup@ensemble.com>
2021-09-01 10:46:17 +0300

    Revert branch to HEAD~3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  displayRecipes = false;
 
-  displayComponent(component: {recipes: boolean, shoppingList: boolean}) {
-    if (component.recipes === true && component.shoppingList === false)
-      this.displayRecipes = true;
-    else if (component.recipes === false && component.shoppingList === true)
-      this.displayRecipes = false;
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,10 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html"
 })
 
-export class HeaderComponent{
+export class HeaderComponent {
     collapsed = true;
-
-    @Output("emitComponent") componentEmitted = new EventEmitter<{recipes: boolean, shoppingList: boolean}>();
-
-    handleNavigation(component: string){
-        if(component === "Recipes")
-            this.componentEmitted.emit({recipes: true, shoppingList: false});
-        else if (component === "Shopping List")
-            this.componentEmitted.emit({recipes: false, shoppingList: true});
-    }
 }
