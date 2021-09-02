41db10c980934bbfeefc1dcd22b5ce08e5759bde
alexandrupp <alexandrup@ensemble.com>
2021-09-02 17:38:38 +0300

    Asgn6. Allowing the user to add Ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() emitIngredient = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createIngredient(name: string, amount: number){
+    this.emitIngredient.emit(new Ingredient(name, amount));
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -13,6 +13,10 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient("Tomatoes", 10)
   ];
 
+  addIngredientToList(ingredient:Ingredient){
+    this.ingredients.push(new Ingredient(ingredient.name, ingredient.amount))
+  }
+
   constructor() { }
 
   ngOnInit(): void {
