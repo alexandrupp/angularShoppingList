dc0dd2746827a08c3bbbb2b3612da6388cfdc842
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-03 18:11:29 +0300

    Asgn8.1 Implement Dropdown Directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,22 @@
-import { Directive} from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-  constructor() { }
+  isDisabled: boolean = true;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener("click") dropdownClick(eventData: Event) {
+    if (this.isDisabled) {
+      this.renderer.addClass(this.elRef.nativeElement, "open");
+    }
+    else {
+      this.renderer.removeClass(this.elRef.nativeElement, "open");
+    }
+    this.isDisabled = !this.isDisabled;
+  }
 
 }
