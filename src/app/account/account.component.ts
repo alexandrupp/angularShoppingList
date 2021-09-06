7aa7180558eb47d054db511b3ae6b750c9146037
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-06 10:10:20 +0300

    9.5 Creating a Data Service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,22 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import {LogginService} from "../logging.service";
+import { AccountsService } from '../accounts.service';
+import { LogginService } from "../logging.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LogginService]
+  providers: [LogginService, AccountsService]
 })
 export class AccountComponent {
 
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string, status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LogginService) { }
+  constructor(private loggingService: LogginService, private accountsService: AccountsService) { }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status);
     this.loggingService.logStatusChange(status);
   }
 
