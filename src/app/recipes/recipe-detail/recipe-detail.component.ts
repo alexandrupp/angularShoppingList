c02eab6b83bb17b8116b8e65ebfcd52cd282cb32
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-06 16:18:26 +0300

    My solution

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,5 +9,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent {
 
   @Input() recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  onSendingToShoppingList() {
+    this.recipe.ingredients.forEach((ingredient) => {
+      this.shoppingListService.addIngredient(ingredient);
+    })
+  }
 
 }
