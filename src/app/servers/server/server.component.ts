a18122bd196b074e52a46bb2a67085f6739663bb
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-07 15:25:25 +0300

    11.18 UsingQueryParameters - Practice

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ServersService } from '../servers.service';
@@ -10,24 +10,24 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   // paramsSubscription: Subscription;
-  
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const id = parseInt(this.route.snapshot.params["id"]);
     this.server = this.serversService.getServer(id);
-    
-    /*this.paramsSubscription = */this.route.params.subscribe(
+
+    this.route.params.subscribe(
       (params: Params) => {
         this.server = this.serversService.getServer(parseInt(params["id"]));
       }
     );
   }
 
-  // ngOnDestroy(){
-  //   this.paramsSubscription.unsubscribe();
-  // }
+  onEdit(){
+    this.router.navigate(["edit"], {relativeTo: this.route});
+  }
 
 }
