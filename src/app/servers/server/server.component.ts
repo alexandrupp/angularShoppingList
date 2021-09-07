cb0f5c266cd92eeeb1be340bd576539b6ca2168c
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-07 14:24:24 +0300

    11.16 Practising and some Common Gotchas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { ServersService } from '../servers.service';
 
@@ -9,11 +11,23 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
-
-  constructor(private serversService: ServersService) { }
+  // paramsSubscription: Subscription;
+  
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = parseInt(this.route.snapshot.params["id"]);
+    this.server = this.serversService.getServer(id);
+    
+    /*this.paramsSubscription = */this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(parseInt(params["id"]));
+      }
+    );
   }
+
+  // ngOnDestroy(){
+  //   this.paramsSubscription.unsubscribe();
+  // }
 
 }
