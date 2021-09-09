d87923e87c410237191636c6d29900a9ea66c35e
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-09 16:23:00 +0300

    11.28 Passing Static Data to a Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 import { ServersService } from './servers/servers.service';
 import { AuthService } from './auth.service';
@@ -26,7 +27,8 @@ import { CanDeactivateGuardService } from './servers/edit-server/can-deactivate-
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
