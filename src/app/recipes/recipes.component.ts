4b1a8860f7550b5de6d904958dbb00c2f88f8cb8
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-29 16:12:20 +0300

    14.1 Improving the Reactive Service w/ Observables

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,15 +8,9 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
 
-  @Input() selectedRecipe: Recipe = new Recipe("", "", "", []);
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
   }
+
 }
