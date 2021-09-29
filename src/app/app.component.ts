3f9a0ea88fad70ea03063942855692ff7ee3ddf8
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-29 18:47:51 +0300

    15.18 TD Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer: string = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: ""
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -29,6 +37,11 @@ export class AppComponent {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
   }
 }
