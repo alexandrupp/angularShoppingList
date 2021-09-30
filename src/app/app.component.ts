b89ecf104b6c19141687eeaa44da85e80845dcc9
Alexandru-Cristian Pop <alexandrup@ensemble.com>
2021-09-30 18:17:46 +0300

    Max's Assignment Solution w/ Async ERR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +9,47 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+// Max's Solution
+  projectForm: FormGroup;
 
-  statuses = ["Stable", "Critical", "Finished"];
-  projectStatusForm: FormGroup;
-  forbiddenProjectNames = ["Test", "abc"];
-
-  ngOnInit() {
-    this.projectStatusForm = new FormGroup({
-      "name": new FormControl(null, [Validators.required, this.forbiddenProjectName.bind(this)]),
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      "projectName": new FormControl(null, [Validators.required, CustomValidators.invalidProjectName]/*, CustomValidators.asyncInvalidProjectName*/),
       "email": new FormControl(null, [Validators.required, Validators.email]),
-      "status": new FormControl(null),
+      "projectStatus": new FormControl("critical"),
     })
   }
 
-  onSubmit() {
-    console.log(this.projectStatusForm);
+  onSaveProject(){
+    console.log(this.projectForm.value);
   }
 
+// Alex's Solution
+  // statuses = ["Stable", "Critical", "Finished"];
+  // projectStatusForm: FormGroup;
+  // forbiddenProjectNames = ["Test", "abc"];
 
-  forbiddenProjectName(control: FormControl): { [s: string]: boolean } | null {
-    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
-      return { "projectNameIsForbidden": true }
-    }
-    return null;
-  }
+  // ngOnInit() {
+  //   this.projectStatusForm = new FormGroup({
+  //     "name": new FormControl(null, [Validators.required, this.forbiddenProjectName.bind(this)]),
+  //     "email": new FormControl(null, [Validators.required, Validators.email]),
+  //     "status": new FormControl(null),
+  //   })
+  // }
 
+  // onSubmit() {
+  //   console.log(this.projectStatusForm);
+  // }
+
+
+  // forbiddenProjectName(control: FormControl): { [s: string]: boolean } | null {
+  //   if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+  //     return { "projectNameIsForbidden": true }
+  //   }
+  //   return null;
+  // }
+
+// Other code
   // genders = ['male', 'female'];
   // signupForm: FormGroup;
   // forbiddenUsernames = ["Chris", "Ana"];
